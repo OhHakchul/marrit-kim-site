5443058d14a7df2af22b118ad760b32e7d94e990
OhHakchul <hakchuloh@gmail.com>
2026-02-25 17:23:48 +0900

    f

--- tina/config.ts
+++ tina/config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "tinacms";
 
-// Hardcoding 'main' to prevent the "process is not defined" error
 const branch = "main";
 
 export default defineConfig({
   branch,
-
-  // Your new keys
   clientId: "d3b7d778-a442-4ca8-a0ca-744680ec24d3",
   token: "e701c499a6cad569edf9d05fe5fa925f05f1eb78",
-
   build: {
     outputFolder: "admin",
     publicFolder: "public",
@@ -31,7 +27,7 @@ export default defineConfig({
           {
             type: "string",
             name: "title",
-            label: "Internal Title",
+            label: "Title",
             isTitle: true,
             required: true,
           },
@@ -39,51 +35,6 @@ export default defineConfig({
             type: "string",
             name: "heroTitle",
             label: "Hero Title",
-          },
-          {
-            type: "string",
-            name: "heroSubtitle",
-            label: "Hero Subtitle",
-          },
-          {
-            type: "string",
-            name: "heroDescription",
-            label: "Hero Description",
-          },
-          {
-            type: "image",
-            name: "aboutImage",
-            label: "About: Portrait Image"
-          },
-          {
-            type: "string",
-            name: "aboutTitle",
-            label: "About: Section Title"
-          },
-          {
-            type: "rich-text",
-            name: "aboutText",
-            label: "About: Text"
-          },
-          {
-            type: "string",
-            name: "bookTitle",
-            label: "Book: Title"
-          },
-          {
-            type: "string",
-            name: "bookQuote",
-            label: "Book: Quote"
-          },
-          {
-            type: "image",
-            name: "bookCover",
-            label: "Book: Cover Image"
-          },
-          {
-            type: "rich-text",
-            name: "bookText",
-            label: "Book: Description"
           },
         ],
       },
