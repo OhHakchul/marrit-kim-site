4b47154a7f3de107e3eb8f8c6776342eeb7336c4
OhHakchul <hakchuloh@gmail.com>
2026-02-25 16:54:41 +0900

    config

--- tina/config.ts
+++ tina/config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tinacms";
 
-// Hardcoding 'main' clears the "Cannot find name 'process'" error
+// Hardcoding 'main' to prevent the "process is not defined" error
 const branch = "main";
 
 export default defineConfig({
   branch,
-  
-  // Replace these with your current active keys from app.tina.io
+
+  // Your new keys
   clientId: "d3b7d778-a442-4ca8-a0ca-744680ec24d3",
   token: "e701c499a6cad569edf9d05fe5fa925f05f1eb78",
 
@@ -31,7 +31,7 @@ export default defineConfig({
           {
             type: "string",
             name: "title",
-            label: "Title",
+            label: "Internal Title",
             isTitle: true,
             required: true,
           },
@@ -49,6 +49,41 @@ export default defineConfig({
             type: "string",
             name: "heroDescription",
             label: "Hero Description",
+          },
+          {
+            type: "image",
+            name: "aboutImage",
+            label: "About: Portrait Image"
+          },
+          {
+            type: "string",
+            name: "aboutTitle",
+            label: "About: Section Title"
+          },
+          {
+            type: "rich-text",
+            name: "aboutText",
+            label: "About: Text"
+          },
+          {
+            type: "string",
+            name: "bookTitle",
+            label: "Book: Title"
+          },
+          {
+            type: "string",
+            name: "bookQuote",
+            label: "Book: Quote"
+          },
+          {
+            type: "image",
+            name: "bookCover",
+            label: "Book: Cover Image"
+          },
+          {
+            type: "rich-text",
+            name: "bookText",
+            label: "Book: Description"
           },
         ],
       },
