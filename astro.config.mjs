bd9dc92294c1f7561b375c112ce19322d5034b8f
Hakchul Oh <hakchuloh@gmail.com>
2026-02-25 23:43:15 +0900

    Fix: Set config for custom domain marritkim.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://ohhakchul.github.io',
-  base: '/marrit-kim-site', // This leading slash is important
+  site: 'https://marritkim.com',
+  base: '/', // This leading slash is important
   integrations: [react()],
 });
